39868825cc96866e31c94792c379296f705b6638
gracealwan <grace.alwan@gmail.com>
2025-12-10 22:17:51 -0500

    env weirdness

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,14 +1,14 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_URL;
 
 if (!supabaseUrl) {
   throw new Error("Missing env var NEXT_PUBLIC_SUPABASE_URL");
 }
 
 if (!supabaseAnonKey) {
-  throw new Error("Missing env var NEXT_PUBLIC_SUPABASE_ANON_KEY");
+  throw new Error("Missing env var NEXT_PUBLIC_SUPABASE_URL");
 }
 
 let browserClient: SupabaseClient | null = null;
