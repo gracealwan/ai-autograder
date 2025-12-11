51fe8ac3a0716438fdfebed11a8f16fdaa6c32a2
gracealwan <grace.alwan@gmail.com>
2025-12-10 22:27:23 -0500

    typo env

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,14 +1,14 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl) {
   throw new Error("Missing env var NEXT_PUBLIC_SUPABASE_URL");
 }
 
 if (!supabaseAnonKey) {
-  throw new Error("Missing env var NEXT_PUBLIC_SUPABASE_URL");
+  throw new Error("Missing env var NEXT_PUBLIC_SUPABASE_ANON_KEY");
 }
 
 let browserClient: SupabaseClient | null = null;
